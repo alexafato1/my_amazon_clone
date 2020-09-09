d4038f558023089680cd1ca28653c3f9121d6bf6
Alexa fato <alexafato@MacBook-Pro-Alexa.local>
2020-09-09 19:15:04 +0300

    2.07

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
-    <Router>
+    <Router >
    
-        <Switch>
+        <Switch >
+        <Route path='/checkout'>
+            <h1>Checkout</h1>
+         </Route>
+         <Route path='/login'>
+            <h1>Login</h1>
+         </Route>
          <Route path='/'>
            <Header/>
-           <h1>Home page</h1>
+           <Home/>
          </Route>
+        
         </Switch>
    
     </Router>
--- src/Header.js
+++ src/Header.js
@@ -2,17 +2,56 @@ import React from 'react';
 import './Header.css';
 import {Link} from 'react-router-dom';
 import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 function Header() {
     return (
         <nav className='header'>
-            <Link to='/login'>
+            {/* logo on the left */}
+            <Link to='/'>
             <img className="header_logo"
             src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
             alt="img"></img>
+
             </Link>
+             {/* Search box */}
+             <div className='header_search'>
             <input type='text' className='header_searchInput'/>
             <SearchIcon className='header_searchIcon'/>
+            </div>
+           
+            <div className='header_nav'>
+               <Link  to='/login' className='header_link' >
+                    <div className='header_option'>
+                            <spam>Hello,</spam>
+                            <spam>Sign in</spam>
+                     </div>
+                </Link>
+               
+                <Link  to='/' className='header_link' >
+                    <div className='header_option'>
+                            <spam>Returms</spam>
+                            <spam>& Orders</spam>
+                     </div>
+                </Link>
+                
+                <Link  to='/' className='header_link' >
+                    <div className='header_option'>
+                            <spam>Your</spam>
+                            <spam>Prime</spam>
+                     </div>
+                </Link>
+               
+                <Link  to='/checkout'  >
+                    <div className='header_optionBasket'>
+                            <ShoppingBasketIcon />
+                            <spam className='header_basketCount'>0</spam>
+                     </div>
+                </Link>
+               
+            </div>
+
+            
         </nav>
     )
 }
